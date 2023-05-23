4be3dc60f6d83b00abdcd717462d2daf4b12763c
arroba <arroba@ttu.ee>
2023-05-23 21:24:23 +0300

    Dto

--- recordstore/src/main/java/ee/taltech/recordstore/controller/SongController.java
+++ recordstore/src/main/java/ee/taltech/recordstore/controller/SongController.java
@@ -1,24 +1,30 @@
 package ee.taltech.recordstore.controller;
 
+import ee.taltech.recordstore.dto.SongDto;
 import ee.taltech.recordstore.repository.SongRepository;
+import ee.taltech.recordstore.service.SongService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/songs?")
+@RequestMapping("api/songs")
 @RequiredArgsConstructor
 
 public class SongController {
 
+    private final SongService songService;
+
     @GetMapping
-    public String getSong(@RequestParam("n") Integer number){
-        System.out.println(number);
-        if(number > 5){
-            return "Hello, Five";
+    public List<SongDto> getSong(@RequestParam(name = "genre", required = false) String genre){
+        if (genre == null || genre == ""){
+            return songService.getAllSongs();
         }
-        return "Hello, whatever";
+        return songService.getSongsByGenre(genre);
     }
 }
---
+++ recordstore/src/main/java/ee/taltech/recordstore/mapper/RecordStoreMapper.java
@@ -0,0 +1,32 @@
+package ee.taltech.recordstore.mapper;
+
+import ee.taltech.recordstore.dto.SongDto;
+import ee.taltech.recordstore.entity.Song;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+
+public class RecordStoreMapper {
+
+    public List<SongDto> mapSongEntitiesToDtos(List<Song> entities) {
+        return entities.stream().map(this::mapSongEntityToDto).collect(Collectors.toList());
+    }
+
+    public SongDto mapSongEntityToDto(Song entity){
+        var dto = new SongDto();
+        dto.setTitle(entity.getTitle());
+        dto.setAlbumName(entity.getAlbum().getTitle());
+        dto.setArtistName(entity.getArtist().getName());
+        dto.setGenre(entity.getGenre().getName());
+        //dto.setLyricText(entity.);
+        //dto.setReleaseDate(entity.getAlbum().getReleaseYear());
+        return dto;
+
+    }
+
+    //Song mapSongDtoToEntity(SongDto dto)
+}
+
